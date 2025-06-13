9a5a9153be41f6c32c16b039ada5f6ec56809fa4
Aryan-B-Parikh <aryan81006@gmail.com>
2025-06-13 20:25:45 +0530

    updated test

--- App.test.js
+++ App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders SoulSync header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/SoulSync/i);
+  expect(titleElement).toBeInTheDocument();
 });
